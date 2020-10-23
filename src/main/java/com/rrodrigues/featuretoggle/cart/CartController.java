7e93e24616b9eb0534d01f5429ea6df9aaba1086
renato <renato.rodriguis01@gmail.com>
2020-10-23 09:57:31 -0300

    refact: reorgazined imports and feature toggles

--- src/main/java/com/rrodrigues/featuretoggle/cart/CartController.java
+++ src/main/java/com/rrodrigues/featuretoggle/cart/CartController.java
@@ -1,6 +1,7 @@
 package com.rrodrigues.featuretoggle.cart;
 
 import lombok.SneakyThrows;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
